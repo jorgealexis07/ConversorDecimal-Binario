530bddea52f6f89f438feef43aa8a741b2afcef9
jorgealexis07 <78820449+jorgealexis07@users.noreply.github.com>
2021-10-06 15:26:39 -0500

    Add files via upload

---
+++ TareaConvert_dec-bin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TareaConvert_dec_bin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            int res=0;
+            string cad="";
+            Console.WriteLine("=============Convercion de Decimal a Binario==============");
+            Console.Write("Ingrese el numero a convertir_ ");
+            Numero = Convert.ToInt32(Console.ReadLine());
+            while(Numero>0)
+            {
+                res = Numero % 2;
+                if(res == 0)
+                {
+                    cad = "0"+cad;
+                }
+                else
+                {
+                    cad = "1"+cad;
+                }
+                Numero =(int)Numero / 2;
+            }
+            Console.Write("El numero en Binario es:_ "+cad+" base2");
+            Console.ReadKey();
+        }
+    }
+}
